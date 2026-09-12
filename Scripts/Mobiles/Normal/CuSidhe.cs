d4111e0778ceff8a61dc2574ef608c74e8e8aef3
Feng-67 <sjd4826@gmail.com>
2026-09-12 20:42:00 +0800

    Removed race restrictions

--- Scripts/Mobiles/Normal/CuSidhe.cs
+++ Scripts/Mobiles/Normal/CuSidhe.cs
@@ -135,23 +135,23 @@ namespace Server.Mobiles
             }
         }
 
-        public override void OnDoubleClick(Mobile from)
-        {
-            if (from.Race != Race.Elf && from == ControlMaster && from.IsPlayer())
-            {
-                Item pads = from.FindItemOnLayer(Layer.Shoes);
+        //public override void OnDoubleClick(Mobile from)
+        //{
+        //    if (from.Race != Race.Elf && from == ControlMaster && from.IsPlayer())
+        //    {
+        //        Item pads = from.FindItemOnLayer(Layer.Shoes);
 
-                if (pads is PadsOfTheCuSidhe)
-                    from.SendLocalizedMessage(1071981); // Your boots allow you to mount the Cu Sidhe.
-                else
-                {
-                    from.SendLocalizedMessage(1072203); // Only Elves may use 
-                    return;
-                }
-            }
+        //        if (pads is PadsOfTheCuSidhe)
+        //            from.SendLocalizedMessage(1071981); // Your boots allow you to mount the Cu Sidhe.
+        //        else
+        //        {
+        //            from.SendLocalizedMessage(1072203); // Only Elves may use 
+        //            return;
+        //        }
+        //    }
 
-            base.OnDoubleClick(from);
-        }
+        //    base.OnDoubleClick(from);
+        //}
 
         public override int GetIdleSound()
         {
